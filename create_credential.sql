38251d657f4f4d30762fa663a0814377248f0c9b
Michael J Gangler <mjgangler@gmail.com>
2021-10-21 13:16:39 -0400

    Delete create_credential.sql

--- create_credential.sql
+++
@@ -1,8 +0,0 @@
-BEGIN
-      DBMS_CLOUD.CREATE_CREDENTIAL(
-        credential_name => 'ATPCS_CRED',
-        username => 'mike@gangler.net',
-        password => 'epzMy-xf;aIYDO35CHL-'
-      );
-END;
-
